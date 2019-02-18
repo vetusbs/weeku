46ad82a103c00e0884487fb4d194c8e6fc619474
antonio lopez <antonio.lopez@metropolis-lab.io>
2019-02-18 18:54:01 +0100

    Fixed lint errors

--- src/main/kotlin/com/weeku/http/MenuController.kt
+++ src/main/kotlin/com/weeku/http/MenuController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class MenuController(
-        private val simpleMenuService: SimpleMenuService
+    private val simpleMenuService: SimpleMenuService
 ) {
 
     @GetMapping("/menu")
--- src/main/kotlin/com/weeku/http/api/MenuApiController.kt
+++ src/main/kotlin/com/weeku/http/api/MenuApiController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MenuApiController(
-        private val simpleMenuService: SimpleMenuService
+    private val simpleMenuService: SimpleMenuService
 ) {
 
     @GetMapping("/api/menu")
