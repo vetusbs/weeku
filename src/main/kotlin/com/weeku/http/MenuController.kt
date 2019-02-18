1fab608d8f991714fe2fc968852e558c555d0b6a
Albert <vetusbs@gmail.com>
2019-02-18 01:24:54 +0100

    Added MVC

---
+++ src/main/kotlin/com/weeku/http/MenuController.kt
@@ -0,0 +1,17 @@
+package com.weeku.http
+
+import com.weeku.domain.services.SimpleMenuService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MenuController(private val simpleMenuService: SimpleMenuService) {
+
+    @GetMapping("/menu")
+    fun getMenu(model: Model): String {
+        model.addAttribute("weeklyMenu", simpleMenuService.generateWeeklyMenu())
+
+        return "menu"
+    }
+}
---
+++ src/main/kotlin/com/weeku/http/api/MenuApiController.kt
@@ -0,0 +1,14 @@
+package com.weeku.http.api
+
+import com.weeku.domain.services.SimpleMenuService
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MenuApiController(private val simpleMenuService: SimpleMenuService) {
+
+    @GetMapping("/api/menu")
+    fun getMenu(model: Model) =
+        simpleMenuService.generateWeeklyMenu()
+}
